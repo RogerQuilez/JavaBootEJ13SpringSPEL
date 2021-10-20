1be4125f5376126e607dddb29aeaca3ef379d61e
Roger <rogeerquilezpalleja@gmail.com>
2021-10-20 13:21:39 +0200

    Refactoring

--- src/main/java/utils/Util.java
+++ src/main/java/utils/Util.java
@@ -9,14 +9,38 @@ public class Util {
 		return (Math.random() * ((max - min) + 1)) + min;
 	}
 	
-	public static String getRandomMarca() {
-		String[] marcas = new String[] {"Acer", "Nvidia", "HP", "ASUS"};
-		int index = (int)Util.generateRandomNumber(0, 3);
-		return marcas[index];
+	public static String getRandomMarcaOrdenador() {
+		String[] marcasOrdenador = new String[] {"HP", "Asus", "Dell", "Acer"};
+		return getElement(marcasOrdenador, marcasOrdenador.length - 1);
+	}
+	
+	public static String getRandomMarcaProcesador() {
+		String[] marcasProcesador = new String[] {"AMD", "Intel", "CML", "NXP"};
+		return getElement(marcasProcesador, marcasProcesador.length - 1);
+	}
+	
+	public static String getRandomMarcaGrafica() {
+		String[] marcasGrafica = new String[] {"AMD", "Nvidia"};
+		return getElement(marcasGrafica, marcasGrafica.length - 1);
+	}
+	
+	public static String getRandomMarcaPerifericos() {
+		String[] marcasPerifericos = new String[] {"Asus", "Acer", "BenQ", "Dell"};
+		return getElement(marcasPerifericos, marcasPerifericos.length - 1);
+	}
+	
+	public static String getRandomMarcaPlacaBase() {
+		String[] marcasPlacaBase = new String[] {"AsRock", "ASUS", "Biostar", "Intell"};
+		return getElement(marcasPlacaBase, marcasPlacaBase.length - 1);
 	}
 	
 	public static String formatNumber(double number) {
 		NumberFormat formatter = new DecimalFormat("#.##");
 		return formatter.format(number);
+	}
+	
+	public static String getElement(String[] marcas, int arrayLength) {
+		int index = (int)Util.generateRandomNumber(0, arrayLength);
+		return marcas[index];
 	}
 }
